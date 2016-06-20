d1a263006e004bdf72a4dd57787ca1bc06156ff8
wangyj62 <wangyingjie62@163.com>
2016-06-20 13:11:01 +0800

    初始提交完成，
    Change-Id: I208547af070d8365d44eb751a684f489d86fcbd7
    Signed-off-by: wangyj62 <wangyingjie62@163.com>

---
+++ src/com/cosmistar/People.java
@@ -0,0 +1,40 @@
+package com.cosmistar;
+
+public class People {
+
+	private String name;
+
+	private int age;
+
+	private String sex;
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
